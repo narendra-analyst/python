0e1546a971b77dd97f13a224ce088fa6d28da743
narendra-analyst <narendra.analyst1506@gmai.com>
2026-02-16 12:00:25 +0530

    string methods

---
+++ python string.py
@@ -0,0 +1,42 @@
+# String Methods
+# Length
+# name='demo'
+# print(len(name))
+#  or
+# nameLength=len(name)
+# print(nameLength)
+
+# Upper case
+# name='narendra'
+# stringUpper=name.upper()
+# print(stringUpper)
+
+# Lower case
+# name='NARENDRA'
+# stringLower=name.lower()
+# print(stringLower)
+
+# Title case
+# movie='jana nayagan'
+# stringTitle=movie.title()
+# print(stringTitle)
+
+# Capitalize
+# name='vijay'
+# stringCapitalize=name.capitalize()
+# print(stringCapitalize)
+
+# Swap case
+# name='thalapathy vijay'
+# stringSwap=name.swapcase()
+# print(stringSwap)
+
+# # Strip Case
+# email=' narendrastark15@gmail.com '
+# stringStrip=email.strip()
+# print(stringStrip)
+
+# Strip Left
+email=' narendrastark15@gmail.com '
+stringLstrip=email.strip()
+print(stringLstrip)
