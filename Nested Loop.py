f4147e2c6c53a35abe7cd5a0a64f41ba2b15497f
narendra-analyst <narendra.analyst1506@gmai.com>
2026-03-02 11:55:45 +0530

    python

--- Nested Loop.py
+++ Nested Loop.py
@@ -1,25 +1,25 @@
-# #1.Star Square Pattern
+#1.Star Square Pattern
 row = 5
 for i in range(row):
     for j in range(row):
         print("*",end=" ")
     print()
 
-# #2.Right Triangle Star Pattern
+#2.Right Triangle Star Pattern
 row = 3
 for i in range(3):
     for j in range(i+1):
         print("*",end=" ")
     print()
 
-# #3.Inverted Right Triangle
+#3.Inverted Right Triangle
 n = 4
 for i in range(n, 0, -1):
     for j in range(i):
         print("*", end=" ")
     print()
 
-# #4.Pyramid Pattern
+#4.Pyramid Pattern
 n = 4
 for i in range(1, n+1):
     print(" " * (n-i), end="")
@@ -27,14 +27,14 @@ for i in range(1, n+1):
         print("* ", end="")
     print()
 
-# #5.Number Triangle Pattern
+#5.Number Triangle Pattern
 n = 4
 for i in range(1, n+1):
     for j in range(1, i+1):
         print(j, end=" ")
     print()
 
-# #6.Floyd’s Triangle
+#6.Floyd’s Triangle
 n = 4
 num = 1
 for i in range(1, n+1):
@@ -43,17 +43,13 @@ for i in range(1, n+1):
         num += 1
     print()
 
-# #7.Diamond Pattern
-n = 4
-
-#8.Upper part
+#7.Diamond Pattern
+#Upper part
 n = 5
 for i in range(1, n+1):
     print(" " * (n-i), end="")
     print("* " * i)
-
-#9.Lower part
-n = 5
+#Lower part
 for i in range(n-1, 0, -1):
     print(" " * (n-i), end="")
     print("* " * i)
