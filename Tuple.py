da1a1c00f260d19737d6feedc423fd76817c2674
narendra-analyst <narendra.analyst1506@gmai.com>
2026-03-05 12:08:35 +0530

    class

---
+++ Tuple.py
@@ -0,0 +1,36 @@
+# # Tuple():
+# # It is used to store multiple values in a single variable.What ever we enter in this (,)bracket it shows the type is tuple.
+# mydata= (10,20,30)
+# print(type(mydata))
+
+# # Ordered it allows the duplicate and we can access this using indexed values.
+# myTuple=(10,20,30,40)
+# print(myTuple[1])
+
+# # Immuatble: You cannot add, remove, or change items after the tuple is created. This ensure data intergrity.
+
+# # Concatenation is used to add the two data in a same tuple.
+# ConcatedData= myTuple+mydata
+# print(ConcatedData)
+
+# # Repetation it repeates the value how many times we mentioned using (*).
+# myName= "Narendra "*3
+# print(myName)
+
+# # Unpacking it means it shows the exact value what we want to print.
+# name,age,city = ('Stark',28, 'New York')
+# print(name)
+
+# # Count - It counts the repeated value.
+# myTuple = (10,20,55,24,60,20)
+# print(myTuple.count(20))
+
+# # Index - It shows the index value for the given number.
+# myNumber = (10,20,30,40,50)
+# print(myNumber.index(10))
+
+# 1.Create a tuple that contains an integer, a string, and a float.
+myTuple = (28,'stark',67.5)
+print(myTuple)
+
+# 
