7adb7c65d8b8b69eaea4c53d1288400e74617569
narendra-analyst <narendra.analyst1506@gmai.com>
2026-03-10 11:58:37 +0530

    set class

--- Set.py
+++ Set.py
@@ -28,18 +28,18 @@ print(mySet)
 # mySet.clear()
 # print(mySet)
 
-# Union() - Returns all element from both sets and also removes the duplicates.
+# Union() - Returns all element from both sets and also removes the duplicates (OR symbol '|').
 Set1 = {10,20,30}
 Set2 = {20,40,50,60}
 print(Set1.union(Set2))
 
-# Intersection - It returns the common data from both the set.
+# Intersection - It returns the common data from both the set (AND symbol '&').
 print(Set1.intersection(Set2))
 
-# Difference() - It returns the different elements in the first set but not in the second set.
+# Difference() - It returns the different elements in the first set but not in the second set (NOT symbol '-').
 print(Set1.difference(Set2))
 
-# Symetric_difference() - It returns the different elements in the both set.
+# Symetric_difference() - It returns the different elements in the both set (XOR symbol '^').
 print(Set1.symmetric_difference(Set2))
 
 # Relationship test methods:
@@ -52,6 +52,56 @@ print({10,20,30,40}.issuperset({20,10,40,70}))
 # isdisjoint() - It returns True if no elements are common in both the set.
 print({'a','b','c'}.isdisjoint({'d','e','f'}))
 
+# 1.Given two lists, return the set of common elements that appear more than once in both lists.
+list1 = [1, 2, 2, 3, 4, 4, 5]
+list2 = [2, 2, 4, 4, 4, 6]
+result = set()
+for i in list1:
+    if list1.count(i) > 1 and list2.count(i) > 1:
+        result.add(i)
+print(result)
 
+# 2.Create a set of unique words from a sentence, where words are separated by spaces and punctuation marks should be ignored.
+sentence = "Hello, world! Hello Python. Python is great!"
+for p in ",.!?:;":
+    sentence = sentence.replace(p, "")
+words = sentence.split()
+unique_words = set(words)
+print(unique_words)
+
+# 3.Write a function that takes a list of numbers and returns the largest subset such that the sum of the subset is even.
+numbers = [3, 5, 7]
+def largest_even_subset(numbers):
+    total = 0
+# find total sum
+    for i in numbers:
+        total = total + i
+# if sum is even return the whole list
+    if total % 2 == 0:
+        return numbers
+# if sum is odd, remove one odd number
+    for i in numbers:
+        if i % 2 != 0:
+            numbers.remove(i)
+            break
+    return numbers
+result = largest_even_subset(numbers)
+print(result)
+
+# 4.Given a set of integers, write a function that returns a new set containing all subsets of the original set.
+numbers = {1, 2, 3}
+def get_subsets(s):
+    subsets = [set()]   # start with empty subset
+    for num in s:
+        new_subsets = []
+        for subset in subsets:
+            new_subset = subset.copy()
+            new_subset.add(num)
+            new_subsets.append(new_subset)
+        subsets.extend(new_subsets)
+    return subsets
+result = get_subsets(numbers)
+for subset in result:
+ print(subset)
 
 
